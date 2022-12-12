aaa39d2eb6fa40db03fe31cb2dfcca708a9efcf7
Victor <manuelher1619@gmail.com>
2022-12-12 15:14:20 -0600

    Ejercicio Reves

---
+++ CH21_EjerciciosReverse/js/main.js
@@ -0,0 +1,12 @@
+//Entrada
+//"Laura Aline"
+//Salida 
+//"enila arual"
+let nombre = "Laura Aline"
+let letra = "";
+for(let i = nombre.length-1; i>=0; i--)
+{
+    letra += nombre[i];
+}
+console.log(letra);
+
