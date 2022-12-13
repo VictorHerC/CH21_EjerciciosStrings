4ff109455c14a3135fabada8a7bce95ac94b3ac1
Victor <manuelher1619@gmail.com>
2022-12-12 23:21:08 -0600

    Se me habia borrado pero ya esta de nuevo

--- CH21_EjerciciosReverse/js/main.js
+++ CH21_EjerciciosReverse/js/main.js
@@ -4,9 +4,16 @@
 //"enila arual"
 let nombre = "Laura Aline"
 let letra = "";
-for(let i = nombre.length-1; i>=0; i--)
+for(let a = nombre.length-1; a>=0; a--)
 {
-    letra += nombre[i];
+    letra += nombre[a];
 }
 console.log(letra);
-
+/*function reves(str){
+    let tmpStr="";
+    for(let indice = str.length-1; indice < 0; index--){
+        tmpStr += str.charAt(indice);      
+    }//for indice
+    return tmpStr;
+}//reves
+//en console se escribe reves("Laura Aline")*/
